6ddadbfcadb39931ee137792492b7698cd0bf35a
kerour amine <amineamin221@gmail.com>
2025-03-06 15:37:31 +0100

    solve problem 4

---
+++ Level1/1-10/4.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+struct stInfo
+{
+    int Age;
+    bool HasDrivingLicense;
+};
+
+stInfo ReadInfo(stInfo Info)
+{
+
+    cout << "Enter your age: \n";
+    cin >> Info.Age;
+    cout << "Do You have driving license ? 1:yes , 0:no\n";
+    cin >> Info.HasDrivingLicense;
+    return Info;
+}
+bool IsAccepted(stInfo Info)
+{
+    if ((Info.Age > 21) && (Info.HasDrivingLicense == 1))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+void PrintResult(bool Result)
+{
+    if (Result)
+    {
+        cout << "Hired\n";
+    }
+    else
+    {
+        cout << "Rejected\n";
+    }
+}
+
+int main()
+{
+    stInfo Info;
+    PrintResult(IsAccepted(ReadInfo(Info)));
+    return 0;
+}
