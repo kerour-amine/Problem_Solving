6fc7297da447a8c30ed8a6bee0b4752a6baf33a6
kerour amine <amineamin221@gmail.com>
2025-03-08 11:16:58 +0100

    solve problem 19

---
+++ Level1/11-20/19.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+float ReadDiameter()
+{
+    float D;
+    cout << "Enter Diameter: ";
+    cin >> D;
+    return D;
+}
+float CircleAreaByDiameter(float D)
+{
+    const float PI = 3.14159265;
+    float Area = (PI * pow(D, 2)) / 4;
+    return Area;
+}
+void PrintResult(float Area)
+{
+    cout << "\nRectangle area is: " << Area << endl;
+}
+int main()
+{
+
+    PrintResult(CircleAreaByDiameter(ReadDiameter()));
+    return 0;
+}
