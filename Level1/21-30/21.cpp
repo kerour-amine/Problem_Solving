615fc845658c5ffd2213eb2c511b5c13449efcf8
kerour amine <amineamin221@gmail.com>
2025-03-09 10:30:36 +0100

    solve problem 21

---
+++ Level1/21-30/21.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+float ReadCircumFerence()
+{
+    float L  ;
+    cout << "Enter L: ";
+    cin >>L;
+    return L;
+}
+float CircleAreaByCircumFerence(float L)
+{
+    const float PI = 3.14159265;
+    float Area = (pow(L,2))/(4*PI);
+    return Area;
+}
+void PrintResult(float Area)
+{
+    cout << "\nRectangle area is: " << Area << endl;
+}
+int main()
+{
+
+    PrintResult(CircleAreaByCircumFerence(ReadCircumFerence()));
+    return 0;
+}
