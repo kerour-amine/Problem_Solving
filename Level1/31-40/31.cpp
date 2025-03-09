b3852ac578a777e78b5f1288bbfe64c1b81d830c
kerour amine <amineamin221@gmail.com>
2025-03-09 14:52:59 +0100

    solve problem 31

---
+++ Level1/31-40/31.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int ReadNumber()
+{
+    int Number;
+    cout << "Please enter number: ";
+    cin >> Number;
+
+    return Number;
+}
+
+int Powerof2_3_4(int N)
+{
+    int P = 1, P1, P2, P3;
+    for (int i = 1; i <= 3; i++)
+    {
+        P = N * P;
+        cout << N << " power of " << i << " = " << P << endl;
+    }
+}
+
+int main()
+{
+    Powerof2_3_4(ReadNumber());
+    return 0;
+}
