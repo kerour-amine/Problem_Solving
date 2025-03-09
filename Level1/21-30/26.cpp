32dccfb7782ba16ee646334aeb14a5c01a19f116
kerour amine <amineamin221@gmail.com>
2025-03-09 12:08:54 +0100

    modify 26.cpp file

--- Level1/21-30/26.cpp
+++ Level1/21-30/26.cpp
@@ -1,40 +1,44 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int ReadNumber(){
+int ReadNumber()
+{
     int Number;
-    cout<<"Please enter a number: ";
-    cin>>Number;
+    cout << "Please enter a number: ";
+    cin >> Number;
     return Number;
 }
-void PrintRangeFrom1ToN_WhileLoop(int N){
-    int c=1;
-    cout<<"Range printed using while :"<<endl;
-    while(c<=N){
-        cout<<c<<endl;
+void PrintRangeFrom1ToN_WhileLoop(int N)
+{
+    int c = 1;
+    cout << "Range printed using while statment: " << endl;
+    while (c <= N)
+    {
+        cout << c << endl;
         c++;
     }
-    cout<<"****************\n";
 }
-void PrintRangeFrom1ToN_DoWhileLoop(int N){
-    int c=1;
-    cout<<"Range printed using do while :"<<endl;
-    do{
-        cout<<c<<endl;
+void PrintRangeFrom1ToN_DoWhileLoop(int N)
+{
+    int c = 1;
+    cout << "Range printed using do while statment: " << endl;
+    do
+    {
+        cout << c << endl;
         c++;
-    }while (c<=N);
-    cout<<"****************\n";
+    } while (c <= N);
 }
-void PrintRangeFrom1ToN_ForLoop(int N){
+void PrintRangeFrom1ToN_ForLoop(int N)
+{
+    cout << "Range printed using for statment:" << endl;
 
-    cout<<"Range printed using for loop :"<<endl;
-
-    for(int i=1;i<=N;i++){
-        cout<<i<<endl;
+    for (int i = 1; i <= N; i++)
+    {
+        cout << i << endl;
     }
-    cout<<"****************\n";
 }
-int main(){
-    int N=ReadNumber();
+int main()
+{
+    int N = ReadNumber();
     PrintRangeFrom1ToN_DoWhileLoop(N);
     PrintRangeFrom1ToN_ForLoop(N);
     PrintRangeFrom1ToN_WhileLoop(N);
