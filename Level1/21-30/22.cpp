f42343f22adbcc48f17f1e96bdf5d61de94fddd8
kerour amine <amineamin221@gmail.com>
2025-03-09 10:52:11 +0100

    modify 22.cpp file

--- Level1/21-30/22.cpp
+++ Level1/21-30/22.cpp
@@ -2,28 +2,28 @@
 #include <math.h>
 using namespace std;
 
-void ReadTriangleData(float &a,float &b)
+void ReadTriangleData(float &a, float &b)
 {
-    
+
     cout << "Enter a: ";
     cin >> a;
     cout << "Enter b: ";
     cin >> b;
-
 }
-float CircleAreaByTriangle(float a,float b)
+float CircleAreaByTriangle(float a, float b)
 {
     const float PI = 3.14159265;
-    float Area = PI*(pow(b,2)/4)*((2*a-b)/(2*a+b)); 
-       return Area;
+    float Area = PI * (pow(b, 2) / 4) * ((2 * a - b) / (2 * a + b));
+    return Area;
 }
 void PrintResult(float Area)
 {
     cout << "\nRectangle area is: " << Area << endl;
 }
 int main()
-{   float a,b;
-    ReadTriangleData(a,b);
-    PrintResult(CircleAreaByTriangle(a,b));
+{
+    float a, b;
+    ReadTriangleData(a, b);
+    PrintResult(CircleAreaByTriangle(a, b));
     return 0;
 }
