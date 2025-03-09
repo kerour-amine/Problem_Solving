675931096bb8c49efdff2374b7012d12c27c1b83
kerour amine <amineamin221@gmail.com>
2025-03-09 10:40:55 +0100

    solve problem 22

---
+++ Level1/21-30/22.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+float ReadTriangleData(float &a,float &b)
+{
+    
+    cout << "Enter a: ";
+    cin >> a;
+    cout << "Enter b: ";
+    cin >> b;
+
+}
+float CircleAreaByTriangle(float a,float b)
+{
+    const float PI = 3.14159265;
+    float Area = PI*(pow(b,2)/4)*((2*a-b)/(2*a+b)); 
+       return Area;
+}
+void PrintResult(float Area)
+{
+    cout << "\nRectangle area is: " << Area << endl;
+}
+int main()
+{   float a,b;
+    ReadTriangleData(a,b);
+    PrintResult(CircleAreaByTriangle(a,b));
+    return 0;
+}
