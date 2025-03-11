74fc6404f1f2e0adb5b4d42abf7d2dd8cac818ac
kerour amine <amineamin221@gmail.com>
2025-03-11 15:51:59 +0100

    solve problem 41

---
+++ Level1/41-50/41.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+float ReadPositiveNumber(string message)
+{
+    float Number = 0;
+    do
+    {
+        cout << message;
+        cin >> Number;
+    } while (Number <= 0);
+
+    return Number;
+}
+
+float HoursToDays(float NumberOfHours)
+{
+    return (float)NumberOfHours / 24;
+}
+float HoursToweeks(float NumberOfHours)
+{
+    return (float)NumberOfHours / 24 / 7;
+}
+float DaysToWeeks(float NumberOfDays)
+{
+    return (float)NumberOfDays / 7;
+}
+
+int main()
+{
+    float NumberOfHours = ReadPositiveNumber("Enter number of hours: ");
+    float NumberOfDays = HoursToDays(NumberOfHours);
+    float NumberOfWeeks = DaysToWeeks(NumberOfDays);
+
+    cout << endl;
+    cout << "Total Hours: " << NumberOfHours << endl;
+    cout << "Total Days: " << NumberOfDays << endl;
+    cout << "Total Weeks: " << HoursToweeks(NumberOfHours) << endl;
+
+    return 0;
+}
