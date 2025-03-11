6cd88b7663ae49a352ff6ada97c8338f94ff8b43
kerour amine <amineamin221@gmail.com>
2025-03-11 17:25:53 +0100

    solve problem 43

---
+++ Level1/41-50/43.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+struct stTaskDuration
+{
+    int NumberOfDays, NumberOfHours, NumberOfMinutes, NumberOfSeconds;
+};
+float ReadPositiveNumber(string message)
+{
+    float Number = 0;
+    do
+    {
+        cout << message;
+        cin >> Number;
+    } while (Number <= 0);
+
+    return Number;
+}
+stTaskDuration SecondsToTaskDuration(int TotalSeconds)
+{
+    stTaskDuration TaskDuration;
+    int SecondsOfDay = 24 * 60 * 60;
+    int SecondsOfHour = 60 * 60;
+    int SecondsOfMinute = 60;
+    int Remainder;
+
+    TaskDuration.NumberOfDays = floor(TotalSeconds / SecondsOfDay);
+    Remainder = TotalSeconds % SecondsOfDay;
+
+    TaskDuration.NumberOfHours = floor(Remainder / SecondsOfHour);
+    Remainder = Remainder % SecondsOfHour;
+
+    TaskDuration.NumberOfMinutes = floor(Remainder / SecondsOfMinute);
+    Remainder = Remainder % SecondsOfMinute;
+    TaskDuration.NumberOfSeconds = Remainder;
+
+    return TaskDuration;
+}
+void PrintTaskDuration(stTaskDuration TaskDuration)
+{
+    cout << TaskDuration.NumberOfDays << ":" << TaskDuration.NumberOfHours << ":"
+         << TaskDuration.NumberOfMinutes << ":" << TaskDuration.NumberOfSeconds;
+}
+
+int main()
+{
+    int TotalSeconds = ReadPositiveNumber("Enter Number Of Seconds: ");
+    PrintTaskDuration(SecondsToTaskDuration(TotalSeconds));
+    return 0;
+}
