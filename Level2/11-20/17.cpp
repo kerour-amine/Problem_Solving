8aee9d4ccb2824f1bcd08b400f1a61fc3aa83faa
kerour amine <amineamin221@gmail.com>
2025-03-14 17:32:43 +0100

    solve problem 17

---
+++ Level2/11-20/17.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+string ReadPassord()
+{
+    string Password = "";
+    cout << "Please enter 3 letters (all capitals): ";
+    cin >> Password;
+    return Password;
+}
+bool GuessPassWord(string Password)
+{
+    cout << "\n";
+    string word = "";
+    int counter = 0;
+    for (int i = 65; i <= 90; i++)
+    {
+
+        for (int j = 65; j <= 90; j++)
+        {
+
+            for (int k = 65; k <= 90; k++)
+            {
+                counter++;
+                word = word + char(i);
+                word = word + char(j);
+                word = word + char(k);
+
+                cout << "Trial 1: " << word << endl;
+                if (Password == word)
+                {
+                    cout << "Password is: " << word << endl;
+                    cout << "Found after " << counter << " Trial(s)" << endl;
+                    return true;
+                }
+                word = "";
+            }
+        }
+    }
+    return false;
+}
+
+int main()
+{
+    GuessPassWord(ReadPassord());
+    return 0;
+}
