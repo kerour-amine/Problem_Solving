f41fe06168ffc9b2de5c8224575d63cf64c723a3
kerour amine <amineamin221@gmail.com>
2025-03-15 17:13:36 +0100

    modify 20.cpp file

--- Level2/11-20/20.cpp
+++ Level2/11-20/20.cpp
@@ -2,12 +2,6 @@
 #include <cstdlib>
 using namespace std;
 
-int RandomNumber(int From, int To)
-{
-    int randNum = rand() % (To - From + 1) + From;
-    return randNum;
-}
-
 enum enCharType
 {
     CapitalLetter = 1,
@@ -16,22 +10,24 @@ enum enCharType
     Digit = 4
 };
 
-char GetRandonCharacter(enCharType CharType)
+int RandomNumber(int From, int To)
+{
+    int randNum = rand() % (To - From + 1) + From;
+    return randNum;
+}
+
+char GetRandomCharacter(enCharType CharType)
 {
     switch (CharType)
     {
     case enCharType::CapitalLetter:
         return char(RandomNumber(65, 90));
-        break;
     case enCharType::SmallLetter:
         return char(RandomNumber(97, 122));
-        break;
     case enCharType::SpecialCharacter:
         return char(RandomNumber(33, 47));
-        break;
     case enCharType::Digit:
         return char(RandomNumber(48, 57));
-        break;
     default:
         return 0;
     }
@@ -41,10 +37,10 @@ int main()
 {
     srand((unsigned)time(NULL));
 
-    cout << GetRandonCharacter(enCharType::CapitalLetter) << endl;
-    cout << GetRandonCharacter(enCharType::SmallLetter) << endl;
-    cout << GetRandonCharacter(enCharType::SpecialCharacter) << endl;
-    cout << GetRandonCharacter(enCharType::Digit) << endl;
+    cout << GetRandomCharacter(enCharType::CapitalLetter) << endl;
+    cout << GetRandomCharacter(enCharType::SmallLetter) << endl;
+    cout << GetRandomCharacter(enCharType::SpecialCharacter) << endl;
+    cout << GetRandomCharacter(enCharType::Digit) << endl;
 
     return 0;
 }
