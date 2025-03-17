92c1670cca97c7c549dd53348d4dfac1cd0e4efa
kerour amine <amineamin221@gmail.com>
2025-03-17 13:42:56 +0100

    solve problem 26

---
+++ Level2/21-30/26.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+int RandomNumber(int From, int To)
+{
+    int randNum = rand() % (To - From + 1) + From;
+
+    return randNum;
+}
+
+void FillArrayWithRandomNumbers(int Arr[100], int &arrLength)
+{
+    cout << "\nEnter Number Of Elements: ";
+    cin >> arrLength;
+
+    for (int i = 0; i < arrLength; i++)
+    {
+        Arr[i] = RandomNumber(1, 100);
+    }
+}
+
+void PrintArray(int Arr[100], int arrLength)
+{
+
+    for (int i = 0; i < arrLength; i++)
+    {
+        cout << Arr[i] << " ";
+    }
+}
+
+int SumArray(int Arr[100], int arrLength)
+{
+    int sum = 0;
+
+    for (int i = 0; i < arrLength; i++)
+    {
+        sum += Arr[i];
+    }
+
+    return sum;
+}
+
+int main()
+{
+    srand((unsigned)time(NULL));
+
+    int Arr[100], arrLength;
+
+    FillArrayWithRandomNumbers(Arr, arrLength);
+
+    cout << "\nArray Elements: ";
+    PrintArray(Arr, arrLength);
+
+    cout << "\nSum of all numbers is: "
+         << SumArray(Arr, arrLength) << endl;
+
+    return 0;
+}
