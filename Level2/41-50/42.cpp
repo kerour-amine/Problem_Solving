698fe062d3b6729702f94d854d1b94b6d1c970f7
kerour amine <amineamin221@gmail.com>
2025-03-18 17:45:52 +0100

    solve problem 42

---
+++ Level2/41-50/42.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+int RandomNumber(int From, int To)
+{
+    int randNum;
+    randNum = rand() % (To - From + 1) + From;
+    return randNum;
+}
+
+void FillArrayWithRandomNumbers(int Arr[100], int &arrLength)
+{
+    cout << "Enter number of elements: ";
+    cin >> arrLength;
+
+    for (int i = 0; i < arrLength; i++)
+    {
+        Arr[i] = RandomNumber(1, 100);
+    }
+}
+
+void PrintArray(int Arr[100], int arrLength)
+{
+
+    for (int i = 0; i < arrLength; i++)
+    {
+        cout << Arr[i] << " ";
+    }
+}
+
+int OddNumbersInArray(int Arr[100], int arrLength)
+{
+    int counter = 0;
+    for (int i = 0; i < arrLength; i++)
+    {
+        if (Arr[i] % 2 != 0)
+            counter++;
+    }
+    return counter;
+}
+
+int main()
+{
+    srand((unsigned)time(NULL));
+
+    int Arr[100], arrLength = 0;
+
+    FillArrayWithRandomNumbers(Arr, arrLength);
+
+    cout << "\nArray Elements: \n";
+    PrintArray(Arr, arrLength);
+
+    cout << "\nOdd Numbers Count is: ";
+    cout << OddNumbersInArray(Arr, arrLength) << endl;
+
+    return 0;
+}
