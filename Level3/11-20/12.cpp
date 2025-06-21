3252632d94036a8cc331a216b263ca0d4eb22e19
Kerour-amine <amineamin221@gmail.com>
2025-06-21 21:04:55 +0100

    compare two matrices element by element

---
+++ Level3/11-20/12.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+using namespace std;
+
+int RandomNumber(int From, int To)
+{
+    int randNum = rand() % (To - From + 1) + From;
+    return randNum;
+}
+
+void FillMatrixWithRandomNumbers(int Matrix[3][3], short Rows, short Cols)
+{
+
+    for (int i = 0; i < Rows; i++)
+    {
+        for (int j = 0; j < Cols; j++)
+        {
+            Matrix[i][j] = RandomNumber(1, 10);
+        }
+    }
+}
+
+int SumOfMatrix(int Matrix[3][3], short Rows, short Cols)
+{
+    int Sum = 0;
+    for (short i = 0; i < Rows; i++)
+    {
+        for (short j = 0; j < Cols; j++)
+        {
+            Sum += Matrix[i][j];
+        }
+    }
+    return Sum;
+}
+
+void PrintMatrix(int Matrix[3][3], short Rows, short Cols)
+{
+
+    for (short i = 0; i < Rows; i++)
+    {
+        for (short j = 0; j < Cols; j++)
+        {
+            printf(" %0*d   ", 2, Matrix[i][j]);
+        }
+        cout << endl;
+    }
+}
+
+bool AreTypicalMatrices(int Matrix1[3][3], int Matrix2[3][3], short Rows, short Cols)
+{
+    for (short i = 0; i < Rows; i++)
+    {
+        for (short j = 0; j < Cols; j++)
+        {
+            if (Matrix1[i][j] != Matrix2[i][j])
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+int main()
+{
+    srand((unsigned)time(NULL));
+
+    int Matrix1[3][3], Matrix2[3][3];
+
+    FillMatrixWithRandomNumbers(Matrix1, 3, 3);
+    cout << "\nMatrix1:\n";
+    PrintMatrix(Matrix1, 3, 3);
+
+    FillMatrixWithRandomNumbers(Matrix2, 3, 3);
+    cout << "\nMatrix2:\n";
+    PrintMatrix(Matrix2, 3, 3);
+
+    if (AreTypicalMatrices(Matrix1, Matrix2, 3, 3))
+        cout << "\nYes: both matrices are typical.";
+    else
+        cout << "\nNo: matrices are NOT typical.";
+
+    return 0;
+}
