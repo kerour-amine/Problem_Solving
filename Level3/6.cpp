198b9650c2b0822378691b7643aad882b5047c3a
Kerour-amine <amineamin221@gmail.com>
2025-06-21 14:07:00 +0100

    fill matrix with ordered numbers then print it

---
+++ Level3/6.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+void FillMatrixWithOrderedNumbers(int arr[3][3], short Rows, short Cols)
+{
+    short Counter = 0;
+    for (short i = 0; i < Rows; i++)
+    {
+        for (short j = 0; j < Cols; j++)
+        {
+            arr[i][j] = ++Counter;
+        }
+    }
+}
+
+void PrintMatrix(int arr[3][3], short Rows, short Cols)
+{
+
+    for (short i = 0; i < Rows; i++)
+    {
+        for (short j = 0; j < Cols; j++)
+        {
+            cout << setw(3) << arr[i][j] << "    ";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+
+    srand((unsigned)time(NULL));
+
+    int arr[3][3];
+    FillMatrixWithOrderedNumbers(arr, 3, 3);
+
+    cout << "The following is 3x3 ordered matrix:" << endl;
+    PrintMatrix(arr, 3, 3);
+}
